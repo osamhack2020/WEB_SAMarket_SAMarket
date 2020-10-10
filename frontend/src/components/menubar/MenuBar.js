1b585f1883d03de2d0e93c52f11bd18b5d4726cc
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-10 14:56:01 +0900

    feat: 메뉴 바 아이콘 및 배치 완료

--- frontend/src/components/menubar/MenuBar.js
+++ frontend/src/components/menubar/MenuBar.js
@@ -4,13 +4,32 @@ import './MenuBar.css'
 
 class MenuBar extends Component {
     state = {
-        keyword: '',
+        clicked: 0,
+    }
+
+    getStyle = (idx) => {
+        const img = {
+            0: 'home', 1: 'shop', 2: 'post', 3: 'adv'
+        }[idx] + (this.state.clicked === idx ?'Click': '')
+        return ({
+            position: 'absolute',
+            top: '20px', left: String(20 + 15*idx) + '%',
+            border: '0px solid',
+            background: 'rgba(0,0,0,0)',
+            backgroundImage: "url(" + require('../../imgs/icons/' +img+ '.png') + ")",
+            backgroundSize: '30px 30px',
+            width: '30px', height: '30px',
+        });
     }
 
     render() {
         return (
             <div className='menuBar'>
-                메뉴바
+                <button style={this.getStyle(0)}/>
+                <button style={this.getStyle(1)}/>
+                <button style={this.getStyle(2)}/>
+                <button style={this.getStyle(3)}/>
+                <button className='writeBtn'/>
             </div>
         );
     }
