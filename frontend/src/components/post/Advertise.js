2de52203b23da29675d4a77877f2d06d9dce2aac
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-12 00:07:53 +0900

    refactor: adv 광고 css 분리

--- frontend/src/components/post/Advertise.js
+++ frontend/src/components/post/Advertise.js
@@ -2,30 +2,19 @@ import React from "react";
 import PostHead from "./PostHead";
 import "./Post.css";
 
-const Advertise = ({ info }) => {
+export default function Advertise({ info }) {
   const { post_id, author, contents } = info;
   const { clr, title } = contents;
   const advStyle = {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: 340,
-    height: 190,
-    border: 0,
-    outline: 0,
-    borderRadius: 20,
-    paddingTop: 10,
-    fontWeight: "bold",
-    fontSize: 25,
     color: clr.font,
     background: `linear-gradient(to bottom, #FDFEFF 1%, ${clr.back})`
   };
   return (
     <div className="adv">
-      <button style={advStyle}>{title}</button>
+      <button style={advStyle} className="advBack">
+        {title}
+      </button>
       <PostHead post_id={post_id} type="post" author={author} />
     </div>
   );
-};
-
-export default Advertise;
+}
