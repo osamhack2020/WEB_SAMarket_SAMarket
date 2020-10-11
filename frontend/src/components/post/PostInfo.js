dc3a3161be588a6f20ced8286c6bede3120f55ea
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-11 10:29:02 +0900

    feat: 광고 게시글 구현

--- frontend/src/components/post/PostInfo.js
+++ frontend/src/components/post/PostInfo.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Advertise from './Advertise'
 import PostHead from './PostHead';
 import Content from './Content'
 import './Post.css';
 
 const PostInfo = ({ info, onSearch }) => {
     const { post_id, author, type, contents } = info;
+    if(type === 'adv') return <Advertise info={info}/>;
     return (
         <div className="info">
             <PostHead
