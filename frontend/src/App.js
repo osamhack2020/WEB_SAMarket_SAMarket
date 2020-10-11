4a763682d4ce00d2be335dad17f948b7ed9e943d
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-11 23:35:56 +0900

    feat: 페이지 라우팅 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Link, } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import SignInPage from './pages/SignInPage';
+/* Index 에서 가장 먼저 호출하는 Component
+사용자 로그인 여부 등을 파악하고, 주소에 따라 화면을 전환함
+*/
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import SignInPage from "./pages/SignInPage";
+import LikesPage from "./pages/LikesPage";
+import ChatsPage from "./pages/ChatsPage";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Route exact path="/" component={MainPage} />
-            <Route path="/signin" component={SignInPage} />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/signin" component={SignInPage} />
+      <Route path="/likes" component={LikesPage} />
+      <Route path="/chats" component={ChatsPage} />
+    </BrowserRouter>
+  );
 }
---
+++ frontend/src/pages/LikesPage.js
@@ -0,0 +1,8 @@
+/* 사용자가 좋아한 게시물을 모아두는 페이지
+로그인 되어 있지 않은 경우, 로그인 요구
+*/
+import React from "react";
+
+export default function LikesPage() {
+  return <div>좋아요</div>;
+}
