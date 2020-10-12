e6e1e4e084f1f003f9dc3f83a0d3641151627c57
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-12 21:11:17 +0900

    refactor: MainPage 함수형으로

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,73 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Header from "../components/header/Header";
 import PostList from "../components/post/PostList";
 import MenuBar from "../components/menubar/MenuBar";
 import defaultState from "../data/posts.json";
 import "./Pages.css";
 
-class MainPage extends Component {
-  state = defaultState;
-  post_id = defaultState.posts.length;
+export default function MainPage() {
+  const [state, setState] = useState(defaultState);
+  const { posts, keyword } = state;
 
-  handleSearch = data => {
+  const handleSearch = data => {
     /* 키워드 변경시 호출 */
-    this.setState({
+    setState({
+      ...state,
       keyword: data.keyword
     });
   };
-
-  handleCreate = data => {
-    /* Post 의 생성시 호출되는 함수
-    post_id 를 1씩 증가시키며 posts 배열에 추가
-    */
-    const { posts } = this.state;
-    this.setState({
-      posts: posts.concat({ post_id: this.post_id++, ...data })
-    });
-  };
-
-  handleRemove = post_id => {
-    /* Post 의 post_id 를 key 로 삭제 */
-    const { posts } = this.state;
-    this.setState({
-      posts: posts.filter(post => post.post_id !== post_id)
-    });
-  };
-
-  handleUpdate = (post_id, data) => {
-    /* post_id 에 해당하는 Post 를 data 의 내용으로 변경 */
-    const { posts } = this.state;
-    this.setState({
-      posts: posts.map(
-        post =>
-          post_id === post.post_id
-            ? { ...post, ...data } // 새 객체를 만들어 할당
-            : post // 기존 값 유지
-      )
-    });
-  };
-
-  render() {
-    const { posts, keyword } = this.state;
-    const filteredList = posts.filter(
-      post =>
-        post.contents.tags.indexOf(keyword) !== -1 ||
-        post.contents.title.indexOf(keyword) !== -1
-    ); // tag 또는 title 에 일치하는 검색어가 있는 것만 표시
-
-    return (
-      <div className="MainPage">
-        <Header onSearch={this.handleSearch} />
-        <PostList
-          posts={filteredList}
-          onRemove={this.handleRemove}
-          onUpdate={this.handleUpdate}
-          onSearch={this.handleSearch}
-        />
-        <MenuBar />
-      </div>
-    );
-  }
+  return (
+    <div className="MainPage">
+      <Header onSearch={handleSearch} />
+      <PostList // tag 또는 title 에 keyword 가 있는 것만 표시
+        posts={posts.filter(
+          post =>
+            post.contents.tags.indexOf(keyword) !== -1 ||
+            post.contents.title.indexOf(keyword) !== -1
+        )}
+        onSearch={handleSearch}
+      />
+      <MenuBar />
+    </div>
+  );
 }
-
-export default MainPage;
