2ed540afae025a8d3e486d073cf289ba9a5fc9e5
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-14 17:11:44 +0900

    feat: NotFound 페이지 구현

--- frontend/src/views/pages/index.js
+++ frontend/src/views/pages/index.js
@@ -2,7 +2,7 @@
 사용자 로그인 여부 등을 파악하고, 주소에 따라 화면을 전환함
 */
 import React from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainPage from "./MainPage";
 import SignInPage from "./SignInPage";
 import LikesPage from "./LikesPage";
@@ -10,18 +10,21 @@ import ChatsPage from "./ChatsPage";
 import WritePage from "./WritePage";
 import PostPage from "./PostPage";
 import ProfilePage from "./ProfilePage";
+import NotFoundPage from "./NotFoundPage";
 
 export default function Pages() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={MainPage} />
-      <Route path="/signin" component={SignInPage} />
-      <Route path="/likes" component={LikesPage} />
-      <Route path="/chats" component={ChatsPage} />
-      <Route path="/write" component={WritePage} />
-      <Route path="/posts" component={PostPage} />
-      <Route path="/profile" component={ProfilePage} />
-      <Route render={() => <div>없는 페이지 입니다.</div>} />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/signin" component={SignInPage} />
+        <Route path="/likes" component={LikesPage} />
+        <Route path="/chats" component={ChatsPage} />
+        <Route path="/write" component={WritePage} />
+        <Route path="/posts/:postId" component={PostPage} />
+        <Route path="/profile" component={ProfilePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
