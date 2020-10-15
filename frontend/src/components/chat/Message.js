6b235528eab6ddeb0e28be794c3c764b54703bcd
KoHyunsu <sce06147@naver.com>
2020-10-15 23:31:02 +0900

    feat: 본인과 상대방의 채팅 메시지 분리

--- frontend/src/components/chat/Message.js
+++ frontend/src/components/chat/Message.js
@@ -4,22 +4,26 @@ import './Chat.css';
 // Material-ui
 import Paper from "@material-ui/core/Paper";
 
-const Message = () => {
-    return (
-        <div>
-            <div className="messageContainer end">
-                <Paper className="messageBox backgroundBlue">
-                    <p className="messageText blue">{text}</p>
-                </Paper>
-                <p className="sentMessage pl-10">{trimmedName}</p>
-            </div>
-            <div className="messageContainer start">
-                <p className="sentMessage pr-10">{user}</p>
-                <Paper className="messageBox backgroundLight">
-                    <p className="messageText black">{text}</p>
-                </Paper>
-            </div>
-        </div>)
+const Message = ({ message: { text, user }, name }) => {
+    let isSentByCurrentUser = false;
+    const trimmedName = name.trim().toLowerCase();
+    if (user === trimmedName) {
+        isSentByCurrentUser = true;
+    } 
+    return isSentByCurrentUser ? (<div className="messageContainer end">
+        <Paper className="messageBox backgroundBlue">
+            <p className="messageText blue">{text}</p>
+        </Paper>
+        <p className="sentMessage pl-10">{trimmedName}</p>
+    </div>)
+        :
+        (<div className="messageContainer start">
+            <p className="sentMessage pr-10">{user}</p>
+            <Paper className="messageBox backgroundLight">
+                <p className="messageText black">{text}</p>
+            </Paper>
+        </div>
+        );
 };
 /*
 class Message extends Component {
