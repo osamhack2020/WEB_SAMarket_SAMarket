6ed49bcddb1e5efd41f6c0df96fb1509448fd58a
KoHyunsu <sce06147@naver.com>
2020-10-15 23:55:56 +0900

    test: 테스트용 더미 데이터 추가

--- frontend/src/components/chat/MessageList.js
+++ frontend/src/components/chat/MessageList.js
@@ -5,6 +5,8 @@ import './Chat.css';
 const MessageList = ({ messages, name }) => {
     return (
         <div className='MessagList'>
+            {/* 더미 데이터 */}
+            <Message message = {{ text:"안녕하세요!", user:"서형진" }} name= {"고현수"} />
             {messages.map((message, i) => (
                 <div key={i}>
                     <Message message={message} name={name} />
