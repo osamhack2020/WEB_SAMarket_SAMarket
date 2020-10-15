a387eb37ea3b851a507ff155b7b017c02113a0fd
KoHyunsu <sce06147@naver.com>
2020-10-15 23:10:21 +0900

    feat: 채팅 페이지에 ChatHeader 컴포넌트 적용

--- frontend/src/pages/ChattingPage.js
+++ frontend/src/pages/ChattingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChatBar from '../components/chat/ChatBar';
+import ChatHeader from '../components/chat/ChatHeader';
 import PostInfo from '../components/post/PostInfo';
 
 const ChattingPage = (props) => {
     return (
         <div className="ChattingPage">
+            <ChatHeader user="" />
             <PostInfo />
             <ChatBar />
         </div>
