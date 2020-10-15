8a124577a172dea2d832692f799bfb5ec7843dcc
KoHyunsu <sce06147@naver.com>
2020-10-15 22:56:34 +0900

    feat: socket.io 모듈을 활용해 소켓연결 구현

---
+++ frontend/src/components/chat/ChatBar.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./Chat.css";
+
+let socket;
+
+const ChatBar = location => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const ENDPOINT = "nanofiber.org:8080/ws";
+  // const ENDPOINT = config.blabla 백 작업 후 추가예정.
+
+  useEffect(() => {
+    // query-string middleware의 사용
+    const { name, room } = queryString.parse(location.search);
+    setName(name);
+    setRoom(room);
+
+    socket = io(ENDPOINT); // 소켓 연결
+
+    socket.emit("join", { name, room }, error => {
+      // console.log("error");
+      // 에러 처리
+      if (error) {
+        alert(error);
+      }
+    });
+
+    // return () => {
+    //   socket.emit("disconnect");
+
+    //   socket.off();
+    // };
+  }, [ENDPOINT, location.search]);
+  // 한번만 부른다
+  // 불필요한 사이드 이펙트를 줄인다.
+
+  return (
+    <div className="chatOuterContainer">
+      <div className="chatInnerContainer">
+        <div className="chatScreen">
+          <div className="chatScreenPaper"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+/*
+class ChatBar extends Component {
+    render() {
+        return (
+            <div className='ChatBar'/>
+        );
+    }
+}
+*/
+export default ChatBar;
