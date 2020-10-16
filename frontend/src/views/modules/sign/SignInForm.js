ee77e6500425c58c794552933d32a0239a462c61
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-16 15:01:56 +0900

    feat: 계정 찾기 기능 구현

--- frontend/src/views/modules/sign/SignInForm.js
+++ frontend/src/views/modules/sign/SignInForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signIn } from "./state";
 import "./Sign.css";
@@ -7,31 +8,40 @@ export default function SignInForm() {
   const [userId, setId] = useState("");
   const [password, setPw] = useState("");
   const dipatch = useDispatch();
+
   const handleSignIn = e => {
     e.preventDefault(); // 리로딩 방지
     dipatch(signIn(userId, password));
   };
 
   return (
-    <form onSubmit={handleSignIn}>
-      <input
-        className="signInput"
-        type="id"
-        placeholder="아이디"
-        value={userId}
-        onChange={e => setId(e.target.value)}
-      />
-      <input
-        className="signInput"
-        type="password"
-        placeholder="비밀번호"
-        value={password}
-        onChange={e => setPw(e.target.value)}
-        onKeyPress={e => (e.key === "Enter" ? handleSignIn(e) : null)}
-      />
-      <button className="signBtn" type="submit">
-        입장하기
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSignIn}>
+        <input
+          className="signInput"
+          type="id"
+          placeholder="아이디"
+          value={userId}
+          onChange={e => setId(e.target.value)}
+        />
+        <input
+          className="signInput"
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={e => setPw(e.target.value)}
+          onKeyPress={e => (e.key === "Enter" ? handleSignIn(e) : null)}
+        />
+        <button className="signBtn" type="submit">
+          입장하기
+        </button>
+      </form>
+      <div className="signQues">
+        문제가 있나요?{" "}
+        <Link to="/sign/findAccount" className="signFindLink">
+          계정찾기
+        </Link>
+      </div>
+    </div>
   );
 }
