8109cb988463282239c73543a2550dc9720a1417
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-16 10:38:59 +0900

    misc: login test 를위한fake signin

--- frontend/src/views/pages/ProfilePage.js
+++ frontend/src/views/pages/ProfilePage.js
@@ -4,19 +4,11 @@ postId를 기준으로 post 를 보여줌
 postId가 없는 경우, 메인화면으로
 */
 import React from "react";
-import { Route } from "react-router-dom";
+import { users } from "../../data/users.json";
 import "./Pages.css";
 
 export default function ProfilePage({ match }) {
   // Routing 만 할 뿐 실제 렌더링은 하지 않음
-  return (
-    <div>
-      <Route path={`${match.url}/:userId`} component={ProfileInfo} />
-    </div>
-  );
-}
-
-function ProfileInfo({ match }) {
-  // userId 로 profile 정보를 렌더링 함
-  return <div>{match.params.userId}</div>;
+  const user = users[match.params.userId];
+  return <div>{user.name}</div>;
 }
