d88c587aa924d83e68b2ff39c384e85d8603d36f
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-17 08:46:27 +0900

    fix: sign up page 짤림 수정

--- frontend/src/views/pages/signPages/SignInPage.js
+++ frontend/src/views/pages/signPages/SignInPage.js
@@ -14,20 +14,22 @@ export default function SignInPage() {
     `;
 
     return (
-      <div className="signPage">
+      <div>
         <div className="signBack" />
-        <div className="signLogo" />
-        <h1 className="signTitle">강군마켓</h1>
-        <h2 className="sginSub">강한군인들의 중고거래 커뮤니티</h2>
-        <SignInForm />
-        <div className="signSpliter" />
-        <SignUpEntry>
-          아직 강군마켓의 일원이 아닌가요?
-          <Link to="signup">
-            <button className="signUpBtn">강한군인으로 등록하기</button>
-          </Link>
-        </SignUpEntry>
-        <AboutUs />
+        <div className="signPage">
+          <div className="signLogo" />
+          <h1 className="signTitle">강군마켓</h1>
+          <h2 className="sginSub">강한군인들의 중고거래 커뮤니티</h2>
+          <SignInForm />
+          <div className="signSpliter" />
+          <SignUpEntry>
+            아직 강군마켓의 일원이 아닌가요?
+            <Link to="signup">
+              <button className="signUpBtn">강한군인으로 등록하기</button>
+            </Link>
+          </SignUpEntry>
+          <AboutUs heightNorm={700} />
+        </div>
       </div>
     );
   }
--- frontend/src/views/pages/signPages/SignUpPage.js
+++ frontend/src/views/pages/signPages/SignUpPage.js
@@ -7,17 +7,19 @@ import SignUpForm from "views/modules/sign/SignUpForm";
 
 export default function SignUpPage() {
   return (
-    <div className="signPage">
+    <div>
       <div className="signBack" />
-      <BackBtn />
-      <Link to="/sign" className="toSignIn">
-        입구로 돌아가기
-      </Link>
-      <div className="signLogo" />
-      <h1 className="signTitle">강군마켓</h1>
-      <h2 className="sginSub">강한군인들의 중고거래 커뮤니티</h2>
-      <SignUpForm />
-      <AboutUs heightNorm={890} />
+      <div className="signPage">
+        <BackBtn />
+        <Link to="/sign" className="toSignIn">
+          입구로 돌아가기
+        </Link>
+        <div className="signLogo" />
+        <h1 className="signTitle">강군마켓</h1>
+        <h2 className="sginSub">강한군인들의 중고거래 커뮤니티</h2>
+        <SignUpForm />
+        <AboutUs />
+      </div>
     </div>
   );
 }
