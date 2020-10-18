52395f2d2f4ebc4bb84b94c3ceffc18c60731692
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-18 11:01:52 +0900

    feat: reply 영역 도달시에만 chat input 뜨도록

--- frontend/src/views/pages/postPages/PostPage.js
+++ frontend/src/views/pages/postPages/PostPage.js
@@ -3,20 +3,17 @@ samarket.kr/posts/${postId} 로
 postId를 기준으로 post 를 보여줌
 postId가 없는 경우, 메인화면으로
 */
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { getPostById } from "views/modules/common/fakeServer";
-import BackBtn from "views/components/header/BackBtn";
 import NotFoundPage from "../tempPages/NotFoundPage";
+import BackBtn from "views/components/header/BackBtn";
 import PostHead from "views/components/post/PostHead";
 import Content from "views/components/post/Content";
 import ChatInput from "views/components/chat/ChatInput";
 
 export default function PostPage({ match }) {
-  const [message, setMessage] = useState("");
   const info = getPostById(match.params.postId);
-  if (!info) {
-    return <NotFoundPage />;
-  }
+  if (!info) return <NotFoundPage />;
   const { postId, author, type } = info;
 
   return (
@@ -26,24 +23,75 @@ export default function PostPage({ match }) {
         <PostHead postId={postId} type={type} author={author} />
       </div>
       <div className="postingInfo">
-        <div className="postingBack">
-          <Content info={info} />
-          <div className="postingContent">
-            {info.contents.content.split("\n").map(line => (
-              <span>
-                {line}
-                <br />
-              </span>
-            ))}
-            <ChatInput
-              message={message}
-              setMessage={setMessage}
-              sendMessage={() => setMessage("")}
-            />
-          </div>
+        <div id="postingContent">
+          <PostingContent info={info} />
         </div>
-        <div className="replies" />
+        <PostingReplies />
       </div>
     </Fragment>
+  );
+}
+
+function PostingContent({ info }) {
+  // Posting 의 글 영역에 해당하는 component
+  return (
+    <div className="postingBack">
+      <Content info={info} />
+      <div className="postingContent">
+        {info.contents.content.split("\n").map(line => (
+          <span>
+            {line}
+            <br />
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function PostingReplies() {
+  // Posting 의 댓글 대댓글 영역
+  const [pageY, setPageY] = useState(0);
+  const [norm, setNorm] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const handleScroll = () => {
+    const { pageYOffset } = window;
+    setPageY(pageYOffset);
+  };
+
+  const calculateNorm = () => {
+    setNorm(
+      document.getElementById("postingContent").clientHeight -
+        document.documentElement.clientHeight +
+        55
+    );
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [pageY]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 화면을 맨 위로
+    calculateNorm();
+  }, []);
+  // 창의 크기가 변하면, 기준도 다시 계산
+  useEffect(() => {
+    window.addEventListener("resize", calculateNorm);
+    return () => window.removeEventListener("resize", calculateNorm);
+  }, [norm]);
+
+  return (
+    <div>
+      <div className="replies">댓글 대댓글 영역</div>
+      {pageY >= norm && (
+        <ChatInput
+          message={message}
+          setMessage={setMessage}
+          sendMessage={() => setMessage("")}
+        />
+      )}
+    </div>
   );
 }
