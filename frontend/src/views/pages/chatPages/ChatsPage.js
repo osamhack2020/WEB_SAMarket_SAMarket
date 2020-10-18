9baa6dfc18d2fb5d49daed9aa048e2e7dca64dad
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-19 01:35:46 +0900

    feat: fakeServer 기본 기능 구축

--- frontend/src/views/pages/chatPages/ChatsPage.js
+++ frontend/src/views/pages/chatPages/ChatsPage.js
@@ -1,14 +1,35 @@
 /* 채팅 목록을 볼 수 있는 페이지
 로그인 되어 있지 않은 경우, 로그인 요구
 */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getChatList } from "views/modules/common/fakeServer";
 
 export default function ChatsPage() {
+  const [chatKeyword, setKeyword] = useState("");
+  const filtered = getChatList().filter(
+    chatInfo =>
+      chatInfo.title.indexOf(chatKeyword) !== -1 ||
+      chatInfo.members.indexOf(chatKeyword) !== -1
+  ); // 채팅방 제목에 검색어가 있거나, 멤버에 검색어가 있는 경우
+
   return (
     <div>
-      <div className="chatsListHead" />
-      <div className="chatsList" />
-      채팅 목록
+      <ChatHead setKeyword={setKeyword} />
+      <div className="chatList">
+        {filtered.map(chatInfo => (
+          <ChatInfo info={chatInfo} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function ChatHead({ setKeyword }) {
+  // 채팅 목록 상단에 있는 검색 가능한 헤드
+  return <div className="chatsListHead" />;
+}
+
+function ChatInfo({ info }) {
+  // 채팅 목록을 보여주는 component
+  return <div />;
 }
