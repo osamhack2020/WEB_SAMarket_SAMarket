ddbb7bea279b447e0305ff1435c2417d0245ad05
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-18 09:21:59 +0900

    feat: backdrop blur 적용

--- frontend/src/views/components/chat/ChatHeader.js
+++ frontend/src/views/components/chat/ChatHeader.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import "./Chat.css";
 import BackBtn from "../header/BackBtn";
+import "./Chat.css";
 
 export default function ChatHeader({ chatRoomId }) {
   // chatRoomTitle 을 얻어오는 logic 필요
   const chatRoomTitle = chatRoomId;
   return (
-    <div className="chatHead">
+    <div className="chatHead backdropBlur">
       <div className="chatTtitle">{chatRoomTitle}</div>
       <BackBtn />
     </div>
