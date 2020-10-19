03a9a4cddcdffd6f6021c164d2881abc5b89dd7d
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-19 10:31:16 +0900

    feat: relay 채팅 profile 숨김

--- frontend/src/views/pages/chatPages/ChatsPage.js
+++ frontend/src/views/pages/chatPages/ChatsPage.js
@@ -1,20 +1,20 @@
 /* 채팅 목록을 볼 수 있는 페이지
 로그인 되어 있지 않은 경우, 로그인 요구
 */
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { getChatList } from "views/modules/common/fakeServer";
 
 export default function ChatsPage() {
   const [chatKeyword, setKeyword] = useState("");
   const filtered = getChatList().filter(
     chatInfo =>
-      chatInfo.title.indexOf(chatKeyword) !== -1 ||
+      chatInfo.chatTitle.indexOf(chatKeyword) !== -1 ||
       chatInfo.members.indexOf(chatKeyword) !== -1
   ); // 채팅방 제목에 검색어가 있거나, 멤버에 검색어가 있는 경우
 
   return (
     <div>
-      <ChatHead setKeyword={setKeyword} />
+      <ChatsHeader setKeyword={setKeyword} />
       <div className="chatList">
         {filtered.map(chatInfo => (
           <ChatInfo info={chatInfo} />
@@ -24,7 +24,7 @@ export default function ChatsPage() {
   );
 }
 
-function ChatHead({ setKeyword }) {
+function ChatsHeader({ setKeyword }) {
   // 채팅 목록 상단에 있는 검색 가능한 헤드
   return <div className="chatsListHead" />;
 }
--- frontend/src/views/pages/chatPages/ChattingPage.js
+++ frontend/src/views/pages/chatPages/ChattingPage.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { getChatRoom } from "views/modules/common/fakeServer";
 import ChatHeader from "views/components/chat/ChatHeader";
 import ChatRoom from "views/components/chat/ChatRoom";
 
 export default function ChattingPage({ match }) {
   const chatRoomId = match.params.chatRoomId;
-  if (useSelector(state => state.sign.authToken)) {
+  const roomInfo = getChatRoom(chatRoomId);
+  const { chatTitle, members } = roomInfo;
+  if (useSelector(state => state.sign.authToken) && members) {
     return (
       <div>
         {/* 더미데이터 서형진 
           => chatRoomId 를 암호화 해서 url로 채팅방 1:1 매칭
          + 해당 채팅방에 들어올 수 있는지 auth 체크 할 것 (backend) */}
-        <ChatHeader chatRoomId={chatRoomId} />
-        <ChatRoom chatRoomId={chatRoomId} />
+        <ChatHeader chatRoomTitle={chatTitle} />
+        <ChatRoom chatRoomId={chatRoomId} roomInfo={roomInfo} />
       </div>
     );
   }
