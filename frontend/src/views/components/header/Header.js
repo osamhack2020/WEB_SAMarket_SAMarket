2b383337dbc8b21f06e0840e8e232b364e6a8fc1
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-20 21:58:02 +0900

    feat: profile 화면에 user profile 뜨도록

--- frontend/src/views/components/header/Header.js
+++ frontend/src/views/components/header/Header.js
@@ -2,21 +2,22 @@
   스크롤에 반응해 크기가 변화
 */
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { changeKeyword } from "views/modules/search/state";
 import Croucher from "../base/Croucher";
 import TopLinks from "./TopLinks";
-import Me from "../me/Me";
 import SearchBar from "./SearchBar";
 import "./Header.css";
+import UserProfile from "../user/UserProfile";
 
 export default function Header() {
+  const userInfo = useSelector(state => state.sign.userInfo);
   const dispatch = useDispatch();
   const onSearch = keyword => dispatch(changeKeyword(keyword));
   return (
     <Croucher norm={200} stretch="head" crouched="crouch">
       <TopLinks />
-      <Me />
+      <UserProfile userInfo={userInfo} />
       <SearchBar onSearch={onSearch} />
     </Croucher>
   );
--- frontend/src/views/pages/ProfilePage.js
+++ frontend/src/views/pages/ProfilePage.js
@@ -6,21 +6,24 @@ postId가 없는 경우, 메인화면으로
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { users } from "data/users.json";
+import { getUserById } from "views/modules/common/fakeServer";
 import { signOut } from "../modules/sign/state";
+import UserProfile from "views/components/user/UserProfile";
 import "./Pages.css";
+import BackBtn from "views/components/header/BackBtn";
 
 export default function ProfilePage({ match }) {
-  const userId = useSelector(state => state.sign.userId);
-  const user = users[match.params.userId];
+  const myId = useSelector(state => state.sign.userId);
+  const user = getUserById(match.params.userId);
   const dispatch = useDispatch();
   const logOut = () => {
     dispatch(signOut());
   };
   return (
-    <div>
-      <div>{user.name}</div>
-      {userId === match.params.userId && (
+    <div className="ProfilePage">
+      <BackBtn />
+      <UserProfile userInfo={user} />
+      {myId === user.id && (
         <Link to="/" onClick={logOut}>
           로그아웃
         </Link>
