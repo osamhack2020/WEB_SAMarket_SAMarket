50594bfbc0ff32de9b9921effedb8b3178b73b43
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-20 16:41:17 +0900

    feat: tag 입력 구현

--- frontend/src/views/components/write/WriteForm.js
+++ frontend/src/views/components/write/WriteForm.js
@@ -35,7 +35,7 @@ export default function WriteForm({ info, updateInfo }) {
         }`}
       />
       <InputTitle># 태그</InputTitle>
-      <TagInput info={info} updateInfo={info} />
+      <TagInput info={info} updateInfo={updateInfo} />
       <InputTitle>색상 설정</InputTitle>
       <div>font back tag 3 가지</div>
       <InputTitle>내용</InputTitle>
@@ -81,22 +81,55 @@ function PostTypeSelector({ updateInfo }) {
 }
 
 function TagInput({ info, updateInfo }) {
+  // 사용자가 태그를 입력하는 영역
   const [curTag, setCurTag] = useState("");
-  const pushTag = () => {};
+
+  const pushTag = () => {
+    updateInfo({
+      tags: info.contents.tags.concat(curTag.slice(0, 24).replace(" ", "_"))
+    });
+    setCurTag("");
+  };
+
+  const popTag = e => {
+    const idx = info.contents.tags.indexOf(e.target.value);
+    info.contents.tags.splice(idx, 1);
+    updateInfo({ tags: info.contents.tags });
+  };
+
   return (
     <div style={{ position: "relative" }}>
       <input
         placeholder="# 태그를 달아주세요."
         value={curTag}
         onChange={e => setCurTag(e.target.value)}
-        className={`postInput tagInput`}
+        onKeyPress={e => (e.key === "Enter" ? pushTag(e) : null)}
+        className={`postInput tagInput ${curTag.length > 24 ? "WARN" : ""}`}
       />
+      {curTag.length > 24 && (
+        <div className="tagWarn">태그는 24글자를 넘을 수 없습니다.</div>
+      )}
       <button
         variant="contained"
         className="btn plusTagBtn"
         type="button"
         onClick={pushTag}
       />
+      <div className="tagContainer">
+        <div className="tagList">태그 목록</div>
+        {info.contents.tags.length === 0 && "태그가 없습니다."}
+        {info.contents.tags.map(tag => (
+          <button
+            className="btn tagRmBtn"
+            onClick={popTag}
+            type="button"
+            value={tag}
+          >
+            <div className="xBlue" />
+            {tag}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
