7dd2f4624b249fc0977ec1f5b4b0efa0075bd690
jkl5616 <jkl5616@gmail.com>
2020-10-20 23:37:43 +0900

    feature: 리뷰 모델 추가

--- backend/models/review.go
+++ backend/models/review.go
@@ -1,1 +1,11 @@
 package models
+
+type Review struct {
+	ID           int
+	Content      string
+	Point        float32
+	Writer       User
+	WriterID     string
+	TargetUser   User
+	TargetUserID string
+}
