f6e8a40a2bb6e9d09b3624d400a49a1c803819e1
JTJun <tae7130@snu.ac.kr>
2020-10-21 18:00:09 +0900

    feat: post list 구현

--- frontend/src/views/components/profile/DealHistory.js
+++ frontend/src/views/components/profile/DealHistory.js
@@ -4,7 +4,7 @@ import HorizontalScroller from "../base/HorizontalScroller";
 export default function DealHistory({ user }) {
   return (
     <HorizontalScroller target="dealHistory" delta={300}>
-      <div className="hScrlTitle?">{user.name}의 강함의 역사</div>
+      <div className="hScrlTitle?">{user.name}의 거래 발자취</div>
     </HorizontalScroller>
   );
 }
--- frontend/src/views/components/profile/PostList.js
+++ frontend/src/views/components/profile/PostList.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { getPostById } from "views/modules/common/fakeServer";
 import HorizontalScroller from "../base/HorizontalScroller";
+import Post from "../post/Post";
 
 export default function PostList({ user }) {
+  const postIds = [5, 5, 1, 1, 5, 5, 1, 5, 1, 5];
   return (
     <HorizontalScroller target="PostList" delta={300}>
-      <div className="hScrlTitle?">{user.name}의 강군로드</div>
+      <div className="hScrlTitle">{user.name}의 강군로드</div>
+      {postIds.map(postId => {
+        const post = getPostById(postId);
+        return (
+          <div
+            className={`postContainer ${post.type === "adv" ? "advPost" : ""}`}
+          >
+            <Post info={post} />
+          </div>
+        );
+      })}
     </HorizontalScroller>
   );
 }
