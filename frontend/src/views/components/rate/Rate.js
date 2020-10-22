324a1eb61bcbf3c9181ff6f6b77a5d9411e9e4d2
JTJun <tae7130@snu.ac.kr>
2020-10-22 17:34:05 +0900

    feat: 평가 전송 버튼

--- frontend/src/views/components/rate/Rate.js
+++ frontend/src/views/components/rate/Rate.js
@@ -12,9 +12,13 @@ export default function Rate({ me, chatRoomId }) {
 
   console.log(seller);
   return (
-    <div className="Rate">
+    <form className="Rate">
+      <div className="rateTitle">{`${
+        seller.id === me.id ? "판매자" : "구매자"
+      }의 평가`}</div>
       <Stars rate={rate} setRate={setRate} />
       <input className="commentInput" placeholder="한줄평을 적어주세요." />
-    </div>
+      <button className="btn commentBtn">거래 완료하기</button>
+    </form>
   );
 }
