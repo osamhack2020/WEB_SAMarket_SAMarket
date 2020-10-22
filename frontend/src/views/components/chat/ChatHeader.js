db9655380fa138f3cda96b373630378ab44095b4
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-22 16:05:09 +0900

    feat: rate 기능 base 작업

--- frontend/src/views/components/chat/ChatHeader.js
+++ frontend/src/views/components/chat/ChatHeader.js
@@ -2,12 +2,19 @@ import React from "react";
 import BackBtn from "../header/BackBtn";
 import "./Chat.css";
 
-export default function ChatHeader({ chatRoomTitle }) {
+export default function ChatHeader({ chatRoomTitle, done, setDone }) {
   // chatRoomTitle 을 얻어오는 logic 필요
   return (
     <div className="chatHead backdropBlur">
       <div className="chatTtitle">{chatRoomTitle}</div>
       <BackBtn />
+      <button
+        className="btn doneBtn"
+        onClick={() => setDone(true)}
+        disabled={done}
+      >
+        {done ? "이미 종료되었습니다" : "거래 종료하기"}
+      </button>
     </div>
   );
 }
