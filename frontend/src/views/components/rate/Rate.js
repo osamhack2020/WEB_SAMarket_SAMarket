d04c7d49fe92fe84fe3cf98a9c60ff597485029a
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-22 17:01:23 +0900

    feat: 채팅방에서 평가하기 구현

--- frontend/src/views/components/rate/Rate.js
+++ frontend/src/views/components/rate/Rate.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  getPostById,
+  getRateByChatRoomId
+} from "views/modules/common/fakeServer";
 import Stars from "./Stars";
 
-export default function Rate({ rateInfo }) {
+export default function Rate({ me, chatRoomId }) {
+  const [rate, setRate] = useState(10);
+  const rateInfo = getRateByChatRoomId(chatRoomId);
+  const seller = getPostById(rateInfo.postId).author;
+
+  console.log(seller);
   return (
-    <div>
-      <Stars rate={5} />
+    <div className="Rate">
+      <Stars rate={rate} setRate={setRate} />
+      <input className="commentInput" placeholder="한줄평을 적어주세요." />
     </div>
   );
 }
