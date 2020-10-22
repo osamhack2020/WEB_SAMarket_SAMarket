f6085861e730371d74fb1682eed873401a6ce282
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-23 02:26:59 +0900

    feat: profile deal history

--- frontend/src/views/components/profile/DealHistory.js
+++ frontend/src/views/components/profile/DealHistory.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { getEmptyPost } from "views/modules/common/fakeServer";
 import HorizontalScroller from "../base/HorizontalScroller";
-import Post from "../post/Post";
+import SAHistory from "../rate/SAHistory";
 
 export default function DealHistory({ user }) {
-  const deals = [];
+  const deals = [0];
   return (
     <HorizontalScroller target="dealHistory" delta={300} margin={0}>
       <div className="hScrlTitle">{user.name}의 거래 발자취</div>
       {deals.length === 0 && (
-        <div className="postContainer emptyPost">
-          <Post info={getEmptyPost(user, "세상에!", "거래내역이 없습니다..")} />
+        <div className="historyContainer">
+          <SAHistory user={user} chatRoomId={-1} />
         </div>
       )}
+      {deals.map(deal => (
+        <div className="historyContainer">
+          <SAHistory user={user} chatRoomId={deal} />
+        </div>
+      ))}
     </HorizontalScroller>
   );
-}
-
-function Deal({ dealId }) {
-  const [seller, buyer] = [null, null];
-  return <div></div>;
 }
