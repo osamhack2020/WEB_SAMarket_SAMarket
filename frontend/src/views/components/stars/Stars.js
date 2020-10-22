bab16a5e9e76ee3f4942a79f8b64c01f3c973119
JTJun <35912840+jtjun@users.noreply.github.com>
2020-10-22 15:46:16 +0900

    feat: 0점 가능하도록

--- frontend/src/views/components/stars/Stars.js
+++ frontend/src/views/components/stars/Stars.js
@@ -1,26 +1,35 @@
 /* 별점 매기기 */
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "./Stars.css";
 
-export default function Star() {
-  const [rate, setRate] = useState(10);
+export default function Star({ rate, setRate, freeze }) {
+  // 1 점을 두 번 누르면 0점
   const setStar = idx => {
     return () => {
       const stars = document.getElementsByClassName("star");
       for (let i = 0; i < 10; i++)
         stars[i].className = stars[i].className.slice(0, 15);
-      for (let i = 0; i <= idx; i++) stars[i].className += " on";
-      setRate(idx + 1);
+      if (!(rate === 1 && idx === 0))
+        for (let i = 0; i <= idx; i++) stars[i].className += " on";
+      if (setRate) setRate(idx + 1);
     };
   };
+
+  useEffect(() => {
+    if (rate !== undefined) setStar(rate - 1)();
+  }, []);
+
   return (
     <div className="Stars">
       {[0, 1, 2, 3, 4].map(idx => (
         <span className="starContainer">
-          <span class="star star_left  on" onClick={setStar(2 * idx)}></span>
+          <span
+            class="star star_left  on"
+            onClick={freeze ? null : setStar(2 * idx)}
+          ></span>
           <span
             class="star star_right on"
-            onClick={setStar(2 * idx + 1)}
+            onClick={freeze ? null : setStar(2 * idx + 1)}
           ></span>
         </span>
       ))}
