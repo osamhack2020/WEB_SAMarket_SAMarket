e2483c24d58b63ddaeb35961ba557dd7c8206898
jkl5616 <jkl5616@gmail.com>
2020-10-25 23:33:18 +0900

    feature: 유저 프로필 API 연동 1/2

--- frontend/src/views/components/header/BackBtn.js
+++ frontend/src/views/components/header/BackBtn.js
@@ -4,19 +4,20 @@ import { withRouter } from "react-router-dom";
 import styled from "styled-components";
 import backSvg from "imgs/icons/back.svg";
 
+const BackBtnStyled = styled.button`
+  width: ${props => props.size ? props.size[0] : 20}px;
+  height: ${props => props.size ? props.size[0] : 20}px;
+  background: url(${backSvg});
+  background-size: 100%;
+  position: ${props => props.fixed ? "fixed" : "absolute"};
+  top: ${props => props.loc ? props.loc[0] : 22}px;
+  left: ${props => props.loc ? props.loc[1] : 10}px;
+  z-index: 9999;
+`;
+
 const BackBtn = ({ history, size, loc, fixed }) => {
   // loc: {top, left}
-  const BackBtn = styled.button`
-    width: ${size ? size[0] : 20}px;
-    height: ${size ? size[0] : 20}px;
-    background: url(${backSvg});
-    background-size: 100%;
-    position: ${fixed ? "fixed" : "absolute"};
-    top: ${loc ? loc[0] : 22}px;
-    left: ${loc ? loc[1] : 10}px;
-    z-index: 9999;
-  `;
-  return <BackBtn className="btn" onClick={e => history.goBack()} />;
+  return <BackBtnStyled size={size} loc={loc} fixed={fixed} className="btn" onClick={e => history.goBack()} />;
 };
 
 export default withRouter(BackBtn);
